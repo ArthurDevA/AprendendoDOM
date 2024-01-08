7b19bea774451b19d25ad3b6a9588c6fa990816a
ArthurDev <arthurdearaujocustodio0@gmail.com>
2024-01-08 20:35:21 -0300

    Calculando e mostrando valores

--- GorjetaRapida/script.js
+++ GorjetaRapida/script.js
@@ -1,11 +1,18 @@
 document.getElementById("conteiner").addEventListener("input", update)
 
 function update() {
-    console.log(
-        {
-            conta: document.getElementById("suaConta").value,
-            gorjeta: document.getElementById("porcentagemGorjeta").value,
-            qtdPessoas: document.getElementById("qtdPessoas").value
-        }
-    )
+    let conta = Number(document.getElementById("suaConta").value)
+    let porcentagemGorjeta = document.getElementById("porcentagemGorjeta").value
+    let qtdPessoas = document.getElementById("qtdPessoas").value
+
+    let gorjeta = conta * porcentagemGorjeta/100
+
+    document.getElementById("gorjetaPorcento").innerHTML = porcentagemGorjeta + "%"
+    document.getElementById("valorGorjeta").innerHTML = gorjeta
+    document.getElementById("valorTotal").innerHTML = conta + gorjeta
+
+    document.getElementById("valorQtdPessoas").innerHTML = qtdPessoas + ((qtdPessoas==1)?" pessoa":" pessoas")
+    document.getElementById("valorDivisaoConta").innerHTML = conta / qtdPessoas
+    document.getElementById("valorDivisaoGorjeta").innerHTML = gorjeta / qtdPessoas
+
 }
