e831c8030e99121065193eb423350c506abd0997
ArthurDev <arthurdearaujocustodio0@gmail.com>
2024-01-08 21:03:24 -0300

    formterdormatando dinheiro

--- GorjetaRapida/script.js
+++ GorjetaRapida/script.js
@@ -1,5 +1,10 @@
 document.getElementById("conteiner").addEventListener("input", update)
 
+function formatarDinheiro(value) {
+    value = (Math.ceil(value * 100) / 100).toFixed(2)
+    return "$ " + value
+}
+
 function update() {
     let conta = Number(document.getElementById("suaConta").value)
     let porcentagemGorjeta = document.getElementById("porcentagemGorjeta").value
@@ -8,11 +13,11 @@ function update() {
     let gorjeta = conta * porcentagemGorjeta/100
 
     document.getElementById("gorjetaPorcento").innerHTML = porcentagemGorjeta + "%"
-    document.getElementById("valorGorjeta").innerHTML = gorjeta
-    document.getElementById("valorTotal").innerHTML = conta + gorjeta
+    document.getElementById("valorGorjeta").innerHTML = formatarDinheiro(gorjeta)
+    document.getElementById("valorTotal").innerHTML = formatarDinheiro(conta + gorjeta)
 
     document.getElementById("valorQtdPessoas").innerHTML = qtdPessoas + ((qtdPessoas==1)?" pessoa":" pessoas")
-    document.getElementById("valorDivisaoConta").innerHTML = conta / qtdPessoas
-    document.getElementById("valorDivisaoGorjeta").innerHTML = gorjeta / qtdPessoas
+    document.getElementById("valorDivisaoConta").innerHTML = formatarDinheiro(conta / qtdPessoas)
+    document.getElementById("valorDivisaoGorjeta").innerHTML = formatarDinheiro(gorjeta / qtdPessoas)
 
 }
